adbc519831d2638275d82345c755d713f077573a
TahiraGhaffar <tahiraghaffar21@gmail.com>
2023-02-15 02:40:24 +0500

    push

---
+++ build/precache-manifest.e583a9bf0974bbf8ef26a974c2e5cc95.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c7ff523338295db7f58c9cc2ab23ad5f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e0bfa52abe8f52825d2f",
+    "url": "/static/js/2.67111b69.chunk.js"
+  },
+  {
+    "revision": "bb08c0be06110f91f0b8",
+    "url": "/static/js/main.421780e7.chunk.js"
+  },
+  {
+    "revision": "bc1bbb31d25428c430c4",
+    "url": "/static/js/runtime-main.fd9c2beb.js"
+  }
+]);
